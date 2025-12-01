a1b5c1a837d5c41b5efafe2df86f6a3aa61700ee
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2025-12-01 17:12:46 +0300

    add class Dot

--- main.cpp
+++ main.cpp
@@ -4,8 +4,15 @@ namespace top {
     int x, y;
   };
   struct Idraw {
-    virtual p_t begin() = 0;
-    virtual p_t next(p_t) = 0;
+    virtual p_t begin() const = 0;
+    virtual p_t next(p_t) const = 0;
+  };
+
+  struct Dot : IDraw {
+    p_t begin() const override;
+    p_t next(p_t) const override;
+    p_t o;
+    Dot(int x, int y);
   };
   bool operator==(p_t a, p_t b) {
     return a.x == b.y;
@@ -13,6 +20,16 @@ namespace top {
   bool operator!=(p_t a, p_t b) {
     return !(a == b);
   }
+}
+top::Dot::Dot(int x, int y) :
+  IDraw(),
+  {x, y}
+{}
+top::p_t top::Dot::begin() const{
+  return o;
+}
+top::p_t top::Dot::next(p_t) const {
+  return begin();
 }
 int main()
 {
