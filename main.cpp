d0e24aa70f19dd0e3eea539fe9d49205a076fad7
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2025-12-01 17:01:47 +0300

    i draw interface

--- main.cpp
+++ main.cpp
@@ -1,2 +1,20 @@
-main()
-{}
+#include <iostream>
+namespace top {
+  struct p_t {
+    int x, y;
+  };
+  struct Idraw {
+    virtual p_t begin() = 0;
+    virtual p_t next(p_t) = 0;
+  };
+  bool operator==(p_t a, p_t b) {
+    return a.x == b.y;
+  }
+  bool operator!=(p_t a, p_t b) {
+    return !(a == b);
+  }
+}
+int main()
+{
+  using namespace top;
+}
